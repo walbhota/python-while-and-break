905e84a1693540b2bce71c2b6fad49bda48421fe
j8 <walbhota@gmail.com>
2021-11-24 01:42:15 +0100

    using while and break statements

---
+++ 4.py
@@ -0,0 +1,8 @@
+while True:
+    print("Please choose an option from the list below")
+    print("Press 1 for selection 1")
+    print("Press 2 for selection 2")
+    print("Press 3 to quit")
+    selection = input("Enter your choice->")
+    if int(selection) == 3:
+        break
